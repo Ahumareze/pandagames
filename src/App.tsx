43a18bf461ec8f77eab6aba2b885afd223fc697f
Ahumareze <ahumarezeifeanyi001@gmail.com>
2022-07-20 18:36:50 +0100

    changed jsx file name to tsx

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+//styles
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello aPP</h1>
+    </div>
+  );
+}
+
+export default App;
