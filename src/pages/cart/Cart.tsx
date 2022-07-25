935dfb85e7b0cac32c86d03ca4f526442e4911f9
Ahumareze <ahumarezeifeanyi001@gmail.com>
2022-07-25 23:53:29 +0100

    updated cart page

--- src/pages/cart/Cart.tsx
+++ src/pages/cart/Cart.tsx
@@ -3,6 +3,9 @@ import React from 'react';
 //styles
 import classes from './cart.module.css';
 
+//utilities
+import { collections } from '../../utilities/links';
+
 function Cart() {
     return (
         <div className={classes.container}>
