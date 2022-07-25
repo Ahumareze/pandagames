685d21fd37be4e2b347a8ce57d826ddfc9b57223
Ahumareze <ahumarezeifeanyi001@gmail.com>
2022-07-26 00:57:33 +0100

    updated cart page

--- src/pages/cart/components/Item/Item.tsx
+++ src/pages/cart/components/Item/Item.tsx
@@ -10,19 +10,27 @@ import banner from '../../../../assets/devImages/games/genshin.jpg';
 import macos from '../../../../assets/icons/macos.png';
 import ps4 from '../../../../assets/icons/ps4.png';
 import windows from '../../../../assets/icons/windows.png';
+import { FiX } from 'react-icons/fi';
 
 function Item() {
     return (
         <div className={classes.container}>
             <img className={classes.mainImage} src={banner} alt=''/>
+
             <div className={classes.details}>
-                <h3>Genshin Impact</h3>
-                <p>$39.99</p>
+                <div>
+                    <h3>Genshin Impact</h3>
+                    <p>$39.99</p>
+                </div>
                 <div className={classes.platforms}>
                     <img alt='' src={ps4} />
                     <img alt='' src={macos} />
                     <img alt='' src={windows} />
                 </div>
+            </div>
+
+            <div className={classes.delete}>
+                <FiX />
             </div>
         </div>
     );
