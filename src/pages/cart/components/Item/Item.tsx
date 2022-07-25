8a62ce3614fa53d137ab148c142af1006fed7f9d
Ahumareze <ahumarezeifeanyi001@gmail.com>
2022-07-26 00:49:13 +0100

    updated cart page

--- src/pages/cart/components/Item/Item.tsx
+++ src/pages/cart/components/Item/Item.tsx
@@ -6,12 +6,23 @@ import classes from './Item.module.css';
 //images
 import banner from '../../../../assets/devImages/games/genshin.jpg';
 
+//icons
+import macos from '../../../../assets/icons/macos.png';
+import ps4 from '../../../../assets/icons/ps4.png';
+import windows from '../../../../assets/icons/windows.png';
+
 function Item() {
     return (
         <div className={classes.container}>
-            <img className={classes.mainImage} src={banner}/>
+            <img className={classes.mainImage} src={banner} alt=''/>
             <div className={classes.details}>
-
+                <h3>Genshin Impact</h3>
+                <p>$39.99</p>
+                <div className={classes.platforms}>
+                    <img alt='' src={ps4} />
+                    <img alt='' src={macos} />
+                    <img alt='' src={windows} />
+                </div>
             </div>
         </div>
     );
