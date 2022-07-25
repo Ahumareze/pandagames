f77f92518b614b8118702e9ad83a6ab3ee06effb
Ahumareze <ahumarezeifeanyi001@gmail.com>
2022-07-26 00:26:24 +0100

    updated cart page

---
+++ src/pages/cart/components/Item/Item.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+//styles
+import classes from './Item.module.css';
+
+function Item() {
+    return (
+        <div className={classes.container}>
+            
+        </div>
+    );
+}
+
+export default Item;
