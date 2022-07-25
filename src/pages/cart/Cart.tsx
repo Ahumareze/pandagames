a329fe9c771636b1d73c7de3916287fc42b30663
Ahumareze <ahumarezeifeanyi001@gmail.com>
2022-07-25 23:52:12 +0100

    added cart page

---
+++ src/pages/cart/Cart.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+//styles
+import classes from './cart.module.css';
+
+function Cart() {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Cart;
