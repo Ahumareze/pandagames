d46edfbfb56c9ac94a6c7f40bec13aa0f6d4c942
Ahumareze <ahumarezeifeanyi001@gmail.com>
2022-07-27 20:55:03 +0100

    added checkout page

---
+++ src/pages/checkout/Checkout.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+//styles
+import classes from ''
+
+function Checkout() {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Checkout;
